ad05183d43d56479e9018dfc653c9f351c0262c1
JUN GYEWON <tonyjev93@gmail.com>
2022-03-14 02:18:11 +0900

    feat: posts 추가 & github blog tips 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "[Github Blog] Tips",
+        "excerpt":"깃허브를 이용한 블로그 만들기 관련 Tips Post 등록 양식 파일명 : YYYY_MM_DD-title.md 내부양식 name mean title 실제 화면에 보이는 제목 categories 포스터의 소속 카테고리 tags 포스터 태그 toc table of content, 우측 상단 목차 노출 여부 toc_label toc 이름 toc_icon toc 아이콘 toc_sticky toc 스크롤 이동시 고정노출 여부(default : true)...","categories": ["Github Blog"],
+        "tags": ["Github Blog","Tip"],
+        "url": "/github%20blog/github-blog-tips/",
+        "teaser": null
+      }]
